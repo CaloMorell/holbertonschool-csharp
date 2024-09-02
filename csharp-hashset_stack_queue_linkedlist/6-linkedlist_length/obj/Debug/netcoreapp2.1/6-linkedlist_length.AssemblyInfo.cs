d051ee5d185cb6a9e5865421967824d74661452b
CaloMorell <oscarmorell07@yahoo.com>
2024-09-01 23:20:07 -0400

    C# - Classes and Namespaces

--- csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/obj/Debug/netcoreapp2.1/6-linkedlist_length.AssemblyInfo.cs
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/obj/Debug/netcoreapp2.1/6-linkedlist_length.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6-linkedlist_length")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e9433f554dc8dee2e33d98b463714bbf14cf130")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95caf5a625772d9d2aba70c83eea2ae2681b4db7")]
 [assembly: System.Reflection.AssemblyProductAttribute("6-linkedlist_length")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6-linkedlist_length")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
